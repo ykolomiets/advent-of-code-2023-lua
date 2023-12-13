3b97ed1edbaddf80301c130dbd52d0142917703e
Yaroslav Kolomiiets <ykolomiets@Yaroslavs-MacBook-Pro.local>
2023-12-13 22:03:04 +0100

    day 13, part 1

---
+++ 13/solve-1.lua
@@ -0,0 +1,69 @@
+local stringx = require("pl.stringx")
+local List = require("pl.List")
+
+local function parse_input()
+  io.input(arg[1])
+  local input = io.read("a")
+  local patterns = stringx.split(input, "\n\n")
+  local encoded_patterns = List()
+  for _, pattern in pairs(patterns) do
+    local horizontal = List()
+    local vertical = List()
+    local lines = stringx.splitlines(pattern)
+    for _ = 1, #lines[1] do
+      vertical:append(0)
+    end
+    for row_number, line in ipairs(lines) do
+      local bitwise_encoded = 0
+      for column_number = 1, #line do
+        local ch = stringx.at(line, column_number)
+        if ch == "#" then
+          bitwise_encoded = bitwise_encoded | (1 << (#line - column_number))
+          vertical[column_number] = vertical[column_number] | (1 << (#lines - row_number))
+        end
+      end
+      horizontal:append(bitwise_encoded)
+    end
+    encoded_patterns:append({pattern = pattern, vertical = vertical, horizontal = horizontal })
+  end
+  return encoded_patterns
+end
+
+local function validate_reflection(values, split_after)
+  local left = split_after
+  local right = split_after + 1
+  while (left > 0) and (right < #values + 1) do
+    if values[left] ~= values[right] then
+      return false
+    end
+    left = left - 1
+    right = right + 1
+  end
+  return true
+end
+
+local function find_reflection_index(values)
+  for i = 1, #values - 1 do
+    if values[i] == values[i + 1] and validate_reflection(values, i) then
+      return i
+    end
+  end
+  return 0
+end
+
+local function main()
+  local patterns = parse_input()
+  local sum_horizontal = patterns
+    :map(function (p)
+      return find_reflection_index(p.horizontal)
+    end)
+    :reduce('+')
+  local sum_vertical = patterns
+    :map(function (p)
+      return find_reflection_index(p.vertical)
+    end)
+    :reduce('+')
+  print(sum_horizontal * 100 + sum_vertical)
+end
+
+main()
